65412862f23f5a56eabc6f1cacfd24f4753bd3c8
LeCreepyyy <42835396+LeCreepyyy@users.noreply.github.com>
2025-04-10 18:54:35 +0200

    clean all file

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-function loadPartial(html_url) {
-    fetch(html_url)
-        .then(response => response.text())
-        .then(html => {
-            const elementHtml = document.getElementById('main-content');
-            if (elementHtml) {
-                elementHtml.innerHTML = html;
-            } else {
-                console.error(`Element can't be loaded. (Url: ${html_url})`);
-            }
-        })
-        .catch(error => {
-            console.error(`Html can't be fetch. (Url: ${html_url})`);
-        });
-}
+function showOncePage(idToShow) {
+    const sections = document.querySelectorAll('.main-content');
+  
+    sections.forEach(section => {
+      section.style.display = (section.id === idToShow) ? 'block' : 'none';
+    });
+  }
 
 window.addEventListener('DOMContentLoaded', () => {
     if (!location.pathname.includes('project')) {
-      loadPartial('partials/home.html');
+        console.log('load home page');
+        showOncePage('home');
     }
-  });
+});
